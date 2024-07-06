62d08b44ed879fe3081a4a06e3bd4d30fda23699
dileepkushwaha <dileepkushwaha@hotmail.com>
2024-07-06 18:37:05 +0530

    added aws credentials

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
 terraform {
+  cloud {
+    organization = "vayoti"
+
+    workspaces {
+      name = "terraform-1"
+    }
+  }
   required_providers {
     random = {
       source  = "hashicorp/random"
       version = "3.6.2"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.57.0"
     }
 
@@ -15,6 +22,14 @@ terraform {
 
 provider "aws" {
   # Configuration options
+  region     = "us-west-2"
+  access_key = "djas;lkjdlas"
+  secret_key = "jklfsjflksjfldskjfslkdjflskdjflksdjf"
+  
+}
+variable "aws_region" {
+  description = "The AWS region to create resources in."
+  default     = "us-west-2"
 }
 provider "random" {
   # Configuration options
@@ -23,8 +38,8 @@ provider "random" {
 resource "random_string" "bucket_name" {
   length  = 16
   special = false
-  lower = true
-  upper = false
+  lower   = true
+  upper   = false
 }
 
 resource "aws_s3_bucket" "example" {
